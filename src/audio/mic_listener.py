4dcaa612f8059e1a0e4c7efa2384ffd262cbbe1c
TEJO SRIDHAR M V S <tejosridhar.mvs@gmail.com>
2025-09-27 23:57:03 +0530

    Update mic_listener.py

--- src/audio/mic_listener.py
+++ src/audio/mic_listener.py
@@ -1,4 +1,10 @@
 import sounddevice as sd
 import numpy as np
 
+class MicListener:
+    def __init__(self, sample_rate=16000, channels=1, chunk_duration_sec=1):
+        self.sample_rate = sample_rate
+        self.channels = channels
+        self.chunk_size = int(sample_rate * chunk_duration_sec)
+
 
