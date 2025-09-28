c1d8534a6e6b30e5ccbe6ff0bee6bc8c7d76120b
TEJO SRIDHAR M V S <tejosridhar.mvs@gmail.com>
2025-09-28 23:58:02 +0530

    Update whisper_asr.py

--- src/speech_to_text/whisper_asr.py
+++ src/speech_to_text/whisper_asr.py
@@ -2,14 +2,3 @@
 import torch
 from transformers import WhisperProcessor, WhisperForConditionalGeneration
 
-class WhisperASR:
-    def __init__(self, model_name="openai/whisper-large-v3-turbo", device="cuda"):
-        self.device = device if torch.cuda.is_available() else "cpu"
-        self.processor = WhisperProcessor.from_pretrained(model_name)
-        self.model = WhisperForConditionalGeneration.from_pretrained(model_name).to(self.device)
-
-    def transcribe(self, audio_chunk):
-        inputs = self.processor(audio_chunk, sampling_rate=16000, return_tensors="pt").to(self.device)
-        generated_ids = self.model.generate(inputs.input_features)
-        transcription = self.processor.decode(generated_ids[0])
-        return transcription
