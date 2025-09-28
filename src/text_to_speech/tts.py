fb14d04a5804eef7ba8d2ac7c4c25eae17d93bc0
TEJO SRIDHAR M V S <tejosridhar.mvs@gmail.com>
2025-09-29 00:00:21 +0530

    Update tts.py

--- src/text_to_speech/tts.py
+++ src/text_to_speech/tts.py
@@ -1,4 +1,3 @@
-
 import requests
 
 class ElevenLabsTTS:
@@ -6,4 +5,10 @@ class ElevenLabsTTS:
         self.api_key = api_key
         self.voice_id = voice_id
         self.api_url = f"https://api.elevenlabs.io/v1/text-to-speech/{self.voice_id}"
+
+    def synthesize(self, text, output_path="output.wav"):
+        headers = {
+            "xi-api-key": self.api_key,
+            "Content-Type": "application/json"
+        }
 
