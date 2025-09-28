e0aa6861d5c8b569cab8248dcb7e420727f4fece
TEJO SRIDHAR M V S <tejosridhar.mvs@gmail.com>
2025-09-29 00:00:01 +0530

    Update tts.py

--- src/text_to_speech/tts.py
+++ src/text_to_speech/tts.py
@@ -1,1 +1,9 @@
 
+import requests
+
+class ElevenLabsTTS:
+    def __init__(self, api_key, voice_id):
+        self.api_key = api_key
+        self.voice_id = voice_id
+        self.api_url = f"https://api.elevenlabs.io/v1/text-to-speech/{self.voice_id}"
+
